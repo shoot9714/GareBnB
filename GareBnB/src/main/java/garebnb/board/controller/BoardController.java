b1f6862f7200c0632c3f06cabbe02561e1daf21a
Jack <Jack@192.168.0.115>
2022-07-12 11:18:14 +0900

    nono

--- GareBnB/src/main/java/garebnb/board/controller/BoardController.java
+++ GareBnB/src/main/java/garebnb/board/controller/BoardController.java
@@ -1,9 +1,13 @@
 package garebnb.board.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import garebnb.board.service.BoardService;
 
@@ -14,4 +18,11 @@ public class BoardController {
 	@Resource(name="BoardService")
 	private BoardService boardService;
 	
+	@RequestMapping(value="/host/mypage/myboardList")
+	public ModelAndView myboardList(Map<String,Object> commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("");
+		log.debug("인터셉트 테스트");
+		
+		return mv;
+	}
 }
