486f6f1c24ae088435a0aaf3a459b535de3cfc19
EZEN_KOO <EZEN@DESKTOP-ID4C0MM>
2022-07-13 14:17:03 +0900

    reservation_dao/impl

--- GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAO.java
+++ GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAO.java
@@ -1,5 +1,18 @@
 package garebnb.reservation.dao;
 
-public interface ReservationDAO {
+import java.util.Map;
 
+public interface ReservationDAO {
+	
+	public Map<String, Object> selectMemReserveList(Map<String, Object> map) throws Exception; 
+
+	public Map<String, Object> selectOneResComList(Map<String, Object> map) throws Exception; 
+	
+	public Map<String, Object> selectReserve(Map<String, Object> map) throws Exception;
+
+	public void updateResReject(Map<String, Object> map) throws Exception;
+
+	public void insertOneReserve(Map<String, Object> map) throws Exception;
+
+	public Map<String, Object> selectReserveDetail(Map<String, Object> map) throws Exception;
 }
--- GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAOImpl.java
+++ GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAOImpl.java
@@ -1,5 +1,7 @@
 package garebnb.reservation.dao;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -7,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
-@Repository("reservationDAO")
+@Repository("ReservationDAO")
 public class ReservationDAOImpl implements ReservationDAO{
 	
 	protected Log log = LogFactory.getLog(ReservationDAO.class);
@@ -20,4 +22,47 @@ public class ReservationDAOImpl implements ReservationDAO{
 			log.debug("\t QueryId \t:  " + queryId);
 		}
 	}
+	//일반회원이 예약내역 보기
+	 @SuppressWarnings("unchecked")
+	    @Override
+	    public Map<String, Object> selectMemReserveList(Map<String, Object> map) throws Exception {
+	        // TODO Auto-generated method stub
+	        return (Map<String , Object>) sqlSession.selectList("reservation.selectMemReserveList", map);
+	 }
+	 
+	 //일반회원이 이용내역 보기
+	 @SuppressWarnings("unchecked")
+	    @Override
+	    public Map<String, Object> selectOneResComList(Map<String, Object> map) throws Exception {
+	        // TODO Auto-generated method stub
+	        return (Map<String , Object>) sqlSession.selectList("reservation.selectOneResComList", map);
+	    }
+	 
+	 //호스트가 예약을 조회하기
+	 @SuppressWarnings("unchecked")
+	    @Override
+	    public Map<String, Object> selectReserve(Map<String, Object> map) throws Exception {
+	        // TODO Auto-generated method stub
+	        return (Map<String , Object>) sqlSession.selectList("reservation.selectReserve", map);
+	    }
+	 
+	 //호스트가 거절사유를 입력
+	 @Override
+	 public void updateResReject(Map<String, Object> map) throws Exception{
+			sqlSession.update("reservation.updateResReject", map);
+		}
+	 
+	 //일반회원이 상세페이지에서 예약요청하기
+	 public void insertOneReserve(Map<String, Object> map) throws Exception{
+			sqlSession.insert("reservation.insertOneReserve", map);
+		}
+	 
+	 //일반회원이 상세페이지에서 예약요청확인전 상세보는 페이지
+	 @SuppressWarnings("unchecked")
+	    @Override
+	    public Map<String, Object> selectReserveDetail(Map<String, Object> map) throws Exception {
+	        // TODO Auto-generated method stub
+	        return (Map<String , Object>) sqlSession.selectList("reservation.selectReserveDetail", map);
+	    }
+
 }
