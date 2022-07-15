d0b4a3677141cac44114cfaa4d5627a564ab9752
kimkibon <darkhalu@naver.com>
2022-07-15 20:57:42 +0900

    오류 수정 var.1

--- GareBnB/src/main/java/garebnb/reservation/controller/ReservationController.java
+++ GareBnB/src/main/java/garebnb/reservation/controller/ReservationController.java
@@ -1,17 +1,17 @@
-package garebnb.reservation.controller;
-
-import javax.annotation.Resource;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-
-import garebnb.reservation.service.ReservationService;
-
-@Controller
-public class ReservationController {
-	
-	Logger log = Logger.getLogger(this.getClass());
-
-	@Resource(name="reservationService")
-	private ReservationService reservationService;
-}
+//package garebnb.reservation.controller;
+//
+//import javax.annotation.Resource;
+//
+//import org.apache.log4j.Logger;
+//import org.springframework.stereotype.Controller;
+//
+//import garebnb.reservation.service.ReservationService;
+//
+//@Controller
+//public class ReservationController {
+//	
+//	Logger log = Logger.getLogger(this.getClass());
+//
+//	@Resource(name="reservationService")
+//	private ReservationService reservationService;
+//}
--- GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAO.java
+++ GareBnB/src/main/java/garebnb/reservation/dao/ReservationDAO.java
@@ -1,18 +1,18 @@
-package garebnb.reservation.dao;
-
-import java.util.Map;
-
-public interface ReservationDAO {
-	
-	public Map<String, Object> selectMemReserveList(Map<String, Object> map) throws Exception; 
-
-	public Map<String, Object> selectOneResComList(Map<String, Object> map) throws Exception; 
-	
-	public Map<String, Object> selectReserve(Map<String, Object> map) throws Exception;
-
-	public void updateResReject(Map<String, Object> map) throws Exception;
-
-	public void insertOneReserve(Map<String, Object> map) throws Exception;
-
-	public Map<String, Object> selectReserveDetail(Map<String, Object> map) throws Exception;
-}
+//package garebnb.reservation.dao;
+//
+//import java.util.Map;
+//
+//public interface ReservationDAO {
+//	
+//	public Map<String, Object> selectMemReserveList(Map<String, Object> map) throws Exception; 
+//
+//	public Map<String, Object> selectOneResComList(Map<String, Object> map) throws Exception; 
+//	
+//	public Map<String, Object> selectReserve(Map<String, Object> map) throws Exception;
+//
+//	public void updateResReject(Map<String, Object> map) throws Exception;
+//
+//	public void insertOneReserve(Map<String, Object> map) throws Exception;
+//
+//	public Map<String, Object> selectReserveDetail(Map<String, Object> map) throws Exception;
+//}
